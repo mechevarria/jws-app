c6c7abc56c45d0a5c61c78171465684d16c95aaf
Mike Echevarria <mechevar@redhat.com>
2018-04-10 16:35:29 -0400

    change return code back to up

--- src/main/java/org/jboss/jws/jwsapp/StatusService.java
+++ src/main/java/org/jboss/jws/jwsapp/StatusService.java
@@ -7,7 +7,7 @@ class StatusService {
     static HashMap<String,String> isUp() {
 
         HashMap<String,String> model = new HashMap<>();
-        model.put("status", "The rest api is now online!");
+        model.put("status", "The rest api is now up!");
 
         return model;
 
