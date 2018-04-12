8db41c7a17aa7d1b816514f4aff8e491aebd7880
Mike Echevarria <mechevar@redhat.com>
2018-04-11 21:48:56 -0400

    use standard web.xml, update to resteasy 3.5.0.FINAL and minor service code formatting

--- src/main/java/org/jboss/jws/jwsapp/StatusService.java
+++ src/main/java/org/jboss/jws/jwsapp/StatusService.java
@@ -8,8 +8,8 @@ class StatusService {
     static HashMap<String,String> isUp() {
 
         HashMap<String,String> model = new HashMap<>();
-        model.put("status", "The Java REST API is now up!");
 
+        model.put("status", "The Java REST API is now up!");
         model.put("time", LocalDateTime.now().toString());
 
         return model;
