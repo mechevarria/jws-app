e552b952a9047dcef60dbf62d82e8f2ecb7aeb88
Mike Echevarria <mechevar@redhat.com>
2018-04-12 11:03:41 -0400

    minor syntax auto formatting

--- src/main/java/org/jboss/jws/jwsapp/StatusService.java
+++ src/main/java/org/jboss/jws/jwsapp/StatusService.java
@@ -5,9 +5,9 @@ import java.util.HashMap;
 
 class StatusService {
 
-    static HashMap<String,String> isUp() {
+    static HashMap<String, String> isUp() {
 
-        HashMap<String,String> model = new HashMap<>();
+        HashMap<String, String> model = new HashMap<>();
 
         model.put("status", "The Java REST API is now up!");
         model.put("time", LocalDateTime.now().toString());
