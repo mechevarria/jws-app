3c75fb01e9b1d5b4b0f984221c0b45262e13d4f6
Mike Echevarria <mechevar@redhat.com>
2019-05-14 13:42:28 -0400

    change app name

--- ocp-cleanup.sh
+++ ocp-cleanup.sh
@@ -3,5 +3,5 @@ oc project pipeline-app-staging
 oc delete all --selector app=jws-app-staging
 
 oc project pipeline-app
-oc delete all --selector app=jws31-tomcat8-basic-s2i
+oc delete all --selector app=jws50-tomcat9-basic-s2i
 oc delete is jws-app-staging
