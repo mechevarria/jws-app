0df5a93a152e278c13b2d849b5dde5d901734e34
Mike Echevarria <mechevar@redhat.com>
2019-05-15 17:02:17 -0400

    go back to original code

--- src/main/java/org/jboss/jws/jwsapp/StatusService.java
+++ src/main/java/org/jboss/jws/jwsapp/StatusService.java
@@ -10,7 +10,6 @@ class StatusService {
         HashMap<String, String> model = new HashMap<>();
 
         model.put("status", "Welcome " + name);
-        model.put("org", "FCC");
         model.put("time", LocalDateTime.now().toString());
 
         return model;
